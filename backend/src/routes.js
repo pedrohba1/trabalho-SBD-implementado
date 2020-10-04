5d641c92023fef49e90d01a85fc69a16d17c9a17
pedro <pedro.almeida@ufu.br>
2020-10-04 16:20:41 -0400

    refactor: removi os arquivos desnecessarios

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,10 @@
 import { Router } from 'express';
 
-
-import UserController from './app/controllers/UserController';
 import PizzaController from './app/controllers/PizzaController';
 import DonoController from './app/controllers/DonoController';
 import PizzariaController from './app/controllers/PizzariaController';
-const routes = new Router();
 
-routes.post('/users', UserController.store);
-routes.put('/users', UserController.update);
+const routes = new Router();
 
 routes.get('/pizzas', PizzaController.index);
 routes.post('/pizzas', PizzaController.store);
